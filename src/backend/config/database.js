1db4b6438abc0237a7d0eb7802d065f3a4617ee5
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-12-20 18:16:38 +0100

    added cron and first pooling

--- src/backend/config/database.js
+++ src/backend/config/database.js
@@ -7,6 +7,26 @@ var database = {
         password: 'swe_project'
     },
 	database: 'swe_project',
-    users_table: 'tbl_benutzer'
+    users_table: 'tbl_benutzer',
+    orders_table: 'tbl_bestellung',
+    ratings_table: 'tbl_ratings',
+    inserat_table: 'tbl_inserat',
+    tbl_plz: 'tbl_plz',
+    tbl_typ: 'tbl_type',
+    tbl_log: 'tbl_log' 
+
 }
+
+
+var pool =    mysql.createPool({
+    connectionLimit : 512, 
+    host     : database.connection.host,
+    user     : database.connection.user,
+    password : database.connection.password,
+    database : database.database,
+    debug    :  false
+});
+
+
+module.exports = pool
 module.exports =  database;
