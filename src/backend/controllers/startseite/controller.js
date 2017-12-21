541c9ee6837331e25d5b1f8d45f0d95fb8332225
Jana <Jana@LAPTOP-7J32LQVM>
2017-12-21 22:08:09 +0100

    error in startseite.ejs

--- src/backend/controllers/startseite/controller.js
+++ src/backend/controllers/startseite/controller.js
@@ -3,24 +3,25 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var dbconfig = require('../../config/database');
 var sanitizer = require('sanitizer');
-
+var bikes;
 module.exports = function(app, passport, verificationMail) {
 	app.get('/', function(req, res) {
-		var bikes;
-		mysqlpool.pool.getConnection(function(err, connection) {
+		
+		mysqlpool.getConnection(function(err, connection) {
 			if (err) {
 				console.log("get bike db failed")
 				return;
 			}
-			//TODO: query anpassen. jede row muss Name, Preis, Rating (avg aller ratings) und Bild (irgendeins random) enthalten
-
 			connection.query("SELECT `Name`, `Price`, AVG(`Rating`), `Picture` FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID` GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25", function(err, rows) {
+				if (err) {
+					console.log("get bike db failed")
+					return;
+				}
 				bikes = rows;
 			});
 			connection.release();
 		});
 
-		console.log("kommt an!");
 		res.render(__dirname + '/startseite.ejs',
 				{
 					bezeichnung : 'trekkingbike',
