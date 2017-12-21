a3d5183494d0f5922df879ae18aec7d6ac4d34a5
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-12-21 20:33:19 +0100

    added base64

--- src/backend/config/passport.js
+++ src/backend/config/passport.js
@@ -58,7 +58,7 @@ module.exports = function(passport, verificationMail) {
             passwordField : 'passwort',
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
-        function(req, email, passwort, done) {
+        function(req,email, passwort, done) {
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
             mysqlpool.pool.getConnection(function(err,connection){
