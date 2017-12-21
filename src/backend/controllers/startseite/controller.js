a7f6e3bd7bb24851d685424c5a56d621d4104e57
Jana <Jana@LAPTOP-7J32LQVM>
2017-12-21 22:32:29 +0100

    bugfix

--- src/backend/controllers/startseite/controller.js
+++ src/backend/controllers/startseite/controller.js
@@ -3,35 +3,37 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var dbconfig = require('../../config/database');
 var sanitizer = require('sanitizer');
-var bikes;
+
 module.exports = function(app, passport, verificationMail) {
 	app.get('/', function(req, res) {
-		
+		var bikes =[];
 		mysqlpool.getConnection(function(err, connection) {
 			if (err) {
 				console.log("get bike db failed")
 				return;
 			}
-			connection.query("SELECT `Name`, `Price`, AVG(`Rating`), `Picture` FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID` GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25", function(err, rows) {
+			connection.query("SELECT `Name`, `Price` as Preis, AVG(`Rating`) * 2 + 0.25 as Rating, `Picture` as Bild FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID` GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25", function(err, rows) {
 				if (err) {
 					console.log("get bike db failed")
 					return;
 				}
 				bikes = rows;
+				console.log(rows[0].Rating)
+				res.render(__dirname + '/startseite.ejs',
+						{
+							bezeichnung : 'trekkingbike',
+							title : 'singlebike',
+							helper : require('../../views/helpers/helper'),
+							layoutPath : '../../views/',
+							isLoggedIn : req.isAuthenticated(),
+							bikes: bikes,
+							loggedIn : true
+						});
 			});
 			connection.release();
 		});
 
-		res.render(__dirname + '/startseite.ejs',
-				{
-					bezeichnung : 'trekkingbike',
-					title : 'singlebike',
-					helper : require('../../views/helpers/helper'),
-					layoutPath : '../../views/',
-					isLoggedIn : req.isAuthenticated(),
-					bikes: bikes,
-					loggedIn : true
-				});
+		
 	});
 	app.get('/startseite/style.css', function(req, res, next) {
 		res.sendfile(__dirname + '/_style.css');
