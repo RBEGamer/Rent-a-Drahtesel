a6199b1eccf2d029d21924f64382cb1381d442fc
Jana <Jana@LAPTOP-7J32LQVM>
2017-12-21 23:35:58 +0100

    adds filter

--- src/backend/controllers/startseite/controller.js
+++ src/backend/controllers/startseite/controller.js
@@ -12,12 +12,13 @@ module.exports = function(app, passport, verificationMail) {
 				console.log("get bike db failed")
 				return;
 			}
-			connection.query("SELECT `Name`, `Price` as Preis, AVG(`Rating`) * 2 + 0.25 as Rating, `Picture` as Bild FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID` GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25", function(err, rows) {
+			connection.query("SELECT `Name`, `Price` as Preis, (AVG(`Rating`)) + 0.5 as Rating, `Picture` as Bild FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID` GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25", function(err, rows) {
 				if (err) {
 					console.log("get bike db failed")
 					return;
 				}
 				bikes = rows;
+				console.log("rating!!! " + rows[0].Rating)
 				res.render(__dirname + '/startseite.ejs',
 						{
 							bezeichnung : 'trekkingbike',
@@ -31,9 +32,49 @@ module.exports = function(app, passport, verificationMail) {
 			});
 			connection.release();
 		});
-
-
 	});
+	
+	app.post('/', function(req, res) {
+		var bikes =[];
+		mysqlpool.getConnection(function(err, connection) {
+			if (err) {
+				console.log("get bike db failed")
+				return;
+			}
+			var query = "SELECT `Name`, `Price` as Preis, (AVG(`Rating`)) + 0.5 as Rating, `Picture` as Bild FROM Fahrrad LEFT JOIN `BewertungFahrrad` ON `BewertungFahrrad`.`pk_ID` = `Fahrrad`.`pk_ID` LEFT JOIN `Bild` ON `Bild`.`ID_Fahrrad` = `Fahrrad`.`pk_ID`";
+			if(req.body.type != null){
+				query += " AND type = " + req.body.type;
+			}
+			if(req.body.price != null){
+				query += " AND price <= " + req.body.price;
+			}
+			if(req.body.size != null){
+				query += " AND size = " + req.body.size;
+			}
+			query += " GROUP BY `Fahrrad`.`pk_ID` ORDER BY `Rating` DESC LIMIT 25"
+			connection.query(query, function(err, rows) {
+				if (err) {
+					console.log("get bike db failed")
+					return;
+				}
+				bikes = rows;
+				console.log("rating!!! " + rows[0].Rating)
+				res.render(__dirname + '/startseite.ejs',
+						{
+							bezeichnung : 'trekkingbike',
+							title : 'singlebike',
+							helper : require('../../views/helpers/helper'),
+							layoutPath : '../../views/',
+							isLoggedIn : req.isAuthenticated(),
+							bikes: bikes,
+							loggedIn : true
+						});
+			});
+			connection.release();
+		});
+		res.send();
+	});
+	
 	app.get('/startseite/style.css', function(req, res, next) {
 		res.sendfile(__dirname + '/_style.css');
 	});
