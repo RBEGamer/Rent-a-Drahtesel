edda2359b779c835fb058f8de68315ec31acddc7
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-12-30 01:38:35 +0100

    PLEASE FIX TODO
    einmal todo mit der Auflistung der bookings machen :D

--- src/backend/controllers/profile/controller.js
+++ src/backend/controllers/profile/controller.js
@@ -63,6 +63,7 @@ module.exports = function(app, passport, verificationMail) {
 
 	app.get('/profile', function(req, res) {
 		var user = req.session.passport.user;
+		var bookings = [];
 		var route = "selfprivatkunde";
 		var query = "SELECT picture`,`Vorname`, `Name`, `phone`, `email`, `city`, `street`, `lat`, `lon`, `housenumber`, `zip`, avg(rating) AS Rating FROM Benutzer AS b JOIN Privatbenutzer AS p ON b.pk_id = p.pk_id JOIN BewertungBenutzer AS bb ON b.pk_id = bb.pk_id WHERE b.pk_id = " + sanitizer.sanitize(user) + " GROUP BY Vorname";
 		console.log(req.session);
@@ -93,7 +94,7 @@ module.exports = function(app, passport, verificationMail) {
 					userdata = rows[0];
 					//TOD IMAGE DATA
 
-
+					
 					//
 					connection.query("SELECT * FROM `BewertungBenutzer`  LEFT JOIN `Benutzer` ON `Benutzer`.`pk_ID` = `BewertungBenutzer`.`pk_ID` WHERE `Benutzer`.`pk_ID` = '" + sanitizer.sanitize(user) +"'", function(err, rows4) {
 						if (err) {
@@ -101,16 +102,37 @@ module.exports = function(app, passport, verificationMail) {
 							res.redirect('/');//TODO ADD FLASH MESSAGE
 							return;
 						}
-
+						
 						connection.query("SELECT * FROM `Fahrrad` WHERE `pk_ID_Benutzer` = '" + sanitizer.sanitize(user) +"'", function(err, rows5) {
 							if (err) {
-								console.log("get user db failed 3");
+								console.log("get user db failed 4");
 								res.redirect('/');//TODO ADD FLASH MESSAGE
 								return;
 							}
+							console.log(rows5);
+							console.log("-------------------------------")
+							
+							rows5.forEach(n => {
+								console.log(n.pk_ID)
+								connection.query("SELECT * FROM `Bestellung` LEFT JOIN `Benutzer` ON `Benutzer`.`pk_ID` = `Bestellung`.`pk_ID_Benutzer` WHERE `pk_ID_Fahrrad` = '" + n.pk_ID + "'", function(err, resu) {
+								if (err) {
+									console.log("get user db failed 5");
+									return;
+								}
+								//TODOFIX
+								console.log(resu);
+								resu.forEach(m => {
+									console.log("bla");
+									bookings.push({booked_days:m.booked_days,userid:m.pk_ID,rent_date:m.Rentdate,email:m.email, bike_id:n.pk_ID})
+									console.log({booked_days:m.booked_days,userid:m.pk_ID,rent_date:m.Rentdate, email:m.email, bike_id:n.pk_ID})
+								});
+							});
+							
+						});
+							
+							//TODO FIX
+							console.log("bk" + bookings)
 
-						//SELECT * FROM `Fahrrad` WHERE `pk_ID_Benutzer` = '15'
-				//console.log(rows4)
 					res.render(__dirname + '/' + route + '.ejs',
 							{
 								helper : require('../../views/helpers/helper'),
@@ -120,9 +142,11 @@ module.exports = function(app, passport, verificationMail) {
 								maps_key: cred.credentials.google_map_api,
 								ratings: rows4,
 								bikes: rows5,
-								userid: sanitizer.sanitize(user)
+								userid: sanitizer.sanitize(user),
+								bookings: bookings
 							});
 						});
+					
 					});
 				});
 			});
