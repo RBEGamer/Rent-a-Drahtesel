ae5cad10c6f6d12740ea6a0b8f788eb4fc6da812
Jana <Jana@LAPTOP-7J32LQVM.fritz.box>
2018-01-19 00:03:39 +0100

    delete user

--- src/backend/controllers/editprofile/controller.js
+++ src/backend/controllers/editprofile/controller.js
@@ -10,87 +10,96 @@ var formvalidator = require('../../config/formvalidator.js');
 var models = require('../../config/models');
 var modelmiddelware = require('../../config/modelmiddelware');
 var formdata = require('../../config/formdata');
-var c= require('../../config/countries');
-
+var c = require('../../config/countries');
 
 module.exports = function(app, passport, verificationMail) {
 
+	app.get('/editprofile', function(req, res, next) {
+		if (!req.isAuthenticated()) {
+			res.redirect('/login');
+			return;
+		}
 
+		var id = req.session.passport.user;
+		models.findSpecialisation([ 'Privatbenutzer', 'Geschaeftsbenutzer' ],
+				'Benutzer', [ "*" ], {
+					pk_ID : id
+				}, function(user) {
 
-app.get('/editprofile', function(req, res, next) {
-    if(!req.isAuthenticated()) {
-        res.redirect('/login');
-        return;
-    }
+					var form = "";
+					if (user.model === "Privatbenutzer") {
+						form = "editprivate";
 
-    var id = req.session.passport.user;
-    models.findSpecialisation(
-        ['Privatbenutzer', 'Geschaeftsbenutzer'], 
-        'Benutzer',
-        ["*"],
-        {pk_ID: id},
-        function(user) {
+					}
 
-            var form = "";
-            if(user.model === "Privatbenutzer"){
-                form = "editprivate";
+					if (user.model === "Geschaeftsbenutzer")
+						form = "editcommercial";
 
-            }
+					var forms = formgenerator.generate([ form ]);
 
-            
-            if(user.model === "Geschaeftsbenutzer") form = "editcommercial";
+					var m = app.locals.formdata
+					var error = (m ? m.error : null);
+					var data = (m ? m.olddata : user.data);
+					app.locals.formdata = null;
+					data["pw"] = "";
+					data["passwortwdh"] = "";
+					res.render(__dirname + '/editprofile.ejs', {
+						layoutPath : '../../views/',
+						isLoggedIn : req.isAuthenticated(),
+						data : data,
+						error : error,
+						forms : forms,
+						model : user.model,
+						start : form,
+						mode : "Update",
+						target : 'editprofile',
+					});
 
-            var forms = formgenerator.generate([form]);
+				});
+	});
 
-            var m =  app.locals.formdata
-            var error = (m ? m.error : null);
-            var data = (m ? m.olddata : user.data);
-            app.locals.formdata = null;
-            data["pw"] = ""; data["passwortwdh"] = "";
-            res.render(__dirname +'/editprofile.ejs', { 
-                layoutPath: '../../views/',
-                isLoggedIn: req.isAuthenticated(),
-                data: data,
-                error: error,
-                forms: forms,
-                model: user.model,
-                start: form,
-                mode: "Update",
-                target: 'editprofile',
-            });
+	app.post('/editprofile', formvalidator.validate, modelmiddelware
+			.hashValue('pw'), function(req, res, next) {
+		console.log('kommt an!');
+		console.log(res.locals);
+		if (res.locals.invalid) {
+			app.locals.formdata = res.locals;
+			res.redirect('/editprofile');
+			return;
+		} else {
 
+			if (req.body.Nachname != null || req.body.Vorname != null) {
+				req.body.name_changed = 1;
+			}
 
-        }
-    );
-});
+			var id = req.session.passport.user;
 
+			models.update(req.body.model, req.body, {
+				pk_ID : id
+			}, function(rows) {
+				res.json(rows);
+			});
 
+		}
+	});
 
-app.post('/editprofile',
-    formvalidator.validate, 
-    modelmiddelware.hashValue('pw'),
-    function(req, res, next) {
-    console.log('kommt an!');
-    console.log(res.locals);
-    if(res.locals.invalid) {
-        app.locals.formdata = res.locals;
-        res.redirect('/editprofile');
-        return;
-    } else {
-
-        if(req.body.Nachname != null || req.body.Vorname != null){
-            req.body.name_changed = 1;
-        }
-        
-        var id = req.session.passport.user;
-
-        models.update(req.body.model, req.body, {pk_ID: id}, function(rows) {
-            res.json(rows);
-        });
-
-    }
-});
+	app.post('/delete', function(req, res, next) {
+		mysqlpool.getConnection(function(err, connection) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var query = "delete from Benutzer where pk_id = " + sanitizer.sanitize(req.session.passport.user);
+			connection.query(query, function(err, rows) {
+				if (err) {
+					console.log("delete failed")
+					return;
+				}
+			});
+			connection.release();
+			req.logout();
+			res.redirect('/');
+		});
+	});
 
 }
-
-
