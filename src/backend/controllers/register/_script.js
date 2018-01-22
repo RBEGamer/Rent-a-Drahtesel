ea7d2a4990d796846894da34de4a404111c47a06
Tobias Arndt <tobias.arndt@rocketmail.com>
2018-01-22 17:42:54 +0100

    registergeschaeft fix

--- src/backend/controllers/register/_script.js
+++ src/backend/controllers/register/_script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+    var datafields = ['registerprivat', 'registercommercial'];
     var mode = ($('#starter').attr('class') === "" ? "registerprivat" : $('#starter').attr('class')); 
-    $('#registercommercial_submit').hide();
+    var pos = (mode === 'registerprivat' ? 0 : 1);
+    $('#chkProfile').prop('checked', (mode === 'registercommercial'));
+    $('#' + datafields[(pos + 1) % 2] + '_submit').hide();
+    $('#' + mode + "_upload").click(submitStuff);
+    
     loadMode();
 
     /*function sendFiles(e) {
@@ -45,8 +50,7 @@ $(document).ready(function () {
     $('#' + mode).submit(sendFiles);*/
    
 
-    var datafields = [];
-
+   
 
 
     function loadMode() {
@@ -63,20 +67,14 @@ $(document).ready(function () {
 
     $('#chkProfile').change(function() {
         console.log("check kommt an!");
-        console.log(mode);
-        if($(this).prop('checked')) {
-            mode = 'registercommercial';
-            $('#registerprivat_submit').hide();
-            $('#registercommercial_submit').show();
-            $('#registerprivat').unbind('submit');
-         //   $('#registercommercial').submit(sendFiles);
-        } else {
-            mode = 'registerprivat';
-            $('#registerprivat_submit').show();
-            $('#registercommercial_submit').hide();
-            $('#registercommercial').unbind('submit');
-           // $('#registerprivat').submit(sendFiles);
-        }
+
+        $("#" + mode + "_submit").hide();
+        $("#" + mode + "_upload").unbind('click');
+        pos = (pos + 1) % 2;
+        mode = datafields[pos];
+        $("#" + mode + "_submit").show();
+        $("#" + mode + "_upload").click(submitStuff);
+
         $('form').each(function() {
             $(this).hide();
         });
@@ -91,38 +89,37 @@ $(document).ready(function () {
             }
         });
         $('#errorField').empty();
+        console.log(mode);
+
     });
 
-    $('#' + mode + "_upload").click(function() {
-        var geocoder = new google.maps.Geocoder();
-        var street = $("#" + mode + " input[name='street']").val();
-        var city = $("#" + mode + " input[name='city']").val();
-        var country = $("#" + mode + " input[name='country'").val();
-        var housenumber = $("#" + mode + " input[name='housenumber']").val();
-        var obj = $(this);
-        var adress = country + " " + city + " " + street + " " + housenumber;
-        geocoder.geocode( { 'address': adress}, function(results, status) {
-            var latitude = "";
-            var longitude = "";
-            console.log("habe lat und lon berechnet!");
-            if (status == google.maps.GeocoderStatus.OK) {
-                latitude = results[0].geometry.location.lat();
-                longitude = results[0].geometry.location.lng();
-                console.log(latitude, longitude);
-            }
-            //obj.append("<input type='hidden' name='lat' value='" + latitude + "' />");
-            //obj.append("<input type='hidden' name='lon' value='" + longitude + "' />");
-            $('#' + mode + " input[name='lon']").attr("value", longitude);
-            $('#' + mode + " input[name='lat']").attr("value", latitude);
+    function submitStuff() {
+            console.log("kommt bei register an!", $(this).attr("id"));
+            var geocoder = new google.maps.Geocoder();
+            var street = $("#" + mode + " input[name='street']").val();
+            var city = $("#" + mode + " input[name='city']").val();
+            var country = $("#" + mode + " input[name='country'").val();
+            var housenumber = $("#" + mode + " input[name='housenumber']").val();
+            var obj = $(this);
+            var adress = country + " " + city + " " + street + " " + housenumber;
+            geocoder.geocode( { 'address': adress}, function(results, status) {
+                var latitude = "";
+                var longitude = "";
+                console.log("habe lat und lon berechnet!");
+                if (status == google.maps.GeocoderStatus.OK) {
+                    latitude = results[0].geometry.location.lat();
+                    longitude = results[0].geometry.location.lng();
+                    console.log(latitude, longitude);
+                }
+                //obj.append("<input type='hidden' name='lat' value='" + latitude + "' />");
+                //obj.append("<input type='hidden' name='lon' value='" + longitude + "' />");
+                $('#' + mode + " input[name='lon']").attr("value", longitude);
+                $('#' + mode + " input[name='lat']").attr("value", latitude);
+    
+                $('#' + mode).submit();
+            });
+    }
 
-            $('#' + mode).submit();
-      
-          
-  
-        });
      
-    });
 
- 
- 
 });
